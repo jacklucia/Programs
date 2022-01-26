0bbba7b898aec3f47e92e0ed1d64f5e7ee925e31
Jack Lucia <jackson.lucia@student.rjuhsd.us>
2022-01-26 12:37:56 -0800

    addded code that doesn't work

--- birthday.py
+++ birthday.py
@@ -1,3 +1,15 @@
-birthday = input("Give me your date of birth now!!(MM/DD/YYYY): ")
-print("Yo birthday is ", birthday)
-HI JACK!!!!
+correctAnswer = False
+notFollowing = 0
+while(correctAnswer == False):
+    birthday = input("Give me your date of birth now!! MM/DD/YYYY: ")
+    if(birthday[1] != '0' or birthday[0] != '1'):
+        print("MM/DD/YYYY FORMAT PLEASE (YES THE FIRST M COUNTS, AND THE FIRST D)\n")
+        notFollowing += 1
+        continue
+    else:
+        correctAnswer = True
+if(notFollowing > 0):
+    print('BRO IT WAS A SIMPLE FORMAT THAT YOU MESSED UP ', str(notFollowing), ' TIMES!!\n')
+    print("ANYWAY, YO BIRTHDAY IS ", birthday)
+else:
+    print("Yo birthday is ", birthday)
